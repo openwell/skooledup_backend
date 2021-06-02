2493c174d372dcfcc698bcd1308991d9b937653d
openwell <timileyinojo@yahoo.com>
2021-06-02 14:49:06 +0100

    knex file fix

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
   //   },
   //   seeds: { directory: './src/db/seeds' },
   // },
-  development: {
+  production: {
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
