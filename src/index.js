8fc97fd835b0824ab0cd98ac177368b4524bb5a1
openwell <timileyinojo@yahoo.com>
2021-06-02 10:12:05 +0100

    dir structure

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ require('@babel/register');
 require('regenerator-runtime/runtime');
 require('dotenv').config(); // load .env variables
 
-let source = process.env.NODE_ENV == 'production' ? './build/app' : './src/app';
-const app = require(source).default;
+const app = require('./app').default;
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
