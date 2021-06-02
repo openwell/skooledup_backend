da11131960e2912593adf57a9ddb8772508e9f95
openwell <timileyinojo@yahoo.com>
2021-06-02 12:14:23 +0100

    knex file fix

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,7 @@
 // Update with your config settings.
 require('dotenv').config();
 module.exports = {
+  // development production
   production: {
     client: 'pg',
     connection: {
@@ -18,36 +19,36 @@ module.exports = {
     },
     seeds: { directory: './src/db/seeds' },
   },
-  development: {
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      database: 'skooledup',
-      user: 'teami',
-      password: '080timi2323',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './src/db/migrations',
-      tableName: 'dbmigrations',
-    },
-    seeds: { directory: './src/db/seeds' },
-  },
-  staging: {
-    client: 'sqlite3',
-    connection: { filename: './src/db/university.db3' },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './src/db/migrations',
-      tableName: 'dbmigrations',
-    },
-    seeds: { directory: './src/db/seeds' },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   tableName: 'knex_migrations'
-    // }
-  },
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     database: 'skooledup',
+  //     user: 'teami',
+  //     password: '080timi2323',
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './src/db/migrations',
+  //     tableName: 'dbmigrations',
+  //   },
+  //   seeds: { directory: './src/db/seeds' },
+  // },
+  // staging: {
+  //   client: 'sqlite3',
+  //   connection: { filename: './src/db/university.db3' },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './src/db/migrations',
+  //     tableName: 'dbmigrations',
+  //   },
+  //   seeds: { directory: './src/db/seeds' },
+  //   // pool: {
+  //   //   min: 2,
+  //   //   max: 10
+  //   // },
+  //   // migrations: {
+  //   //   tableName: 'knex_migrations'
+  //   // }
+  // },
 };
