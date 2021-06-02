b90274d438bb3b3eeb63d4064abd00d5f5dfc7b4
openwell <timileyinojo@yahoo.com>
2021-06-02 14:24:56 +0100

    knex file fix

--- knexfile.js
+++ knexfile.js
@@ -18,11 +18,10 @@ module.exports = {
   // },
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    // connection: {
-    //   connectionString: process.env.DATABASE_URL,
-    //   ssl: { rejectUnauthorized: false },
-    // },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     // pool: {
     //   min: 2,
     //   max: 10,
